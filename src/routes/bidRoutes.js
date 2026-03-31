422adadde537f6cd0864e551d74c623d0d8106c7
Hasitha Prabath <hpkatugampola2002@gmail.com>
2026-03-31 22:48:20 +0530

    change bid routes

--- src/routes/bidRoutes.js
+++ src/routes/bidRoutes.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const { body } = require('express-validator');
-const { authMiddleware } = require('../middleware/authMiddleware');
-const { placeBid, updateBid, getBidStatus, getMonthlyStatus } = require('../controllers/bidController');
+const express = require("express");
+const { body } = require("express-validator");
+const { authMiddleware } = require("../middleware/authMiddleware");
+const {
+  placeBid,
+  updateBid,
+  getBidStatus,
+  getMonthlyStatus,
+} = require("../controllers/bidController");
 
 const router = express.Router();
 
@@ -38,9 +43,17 @@ router.use(authMiddleware);
  *       403:
  *         description: Monthly winning limit reached.
  */
-router.post('/', [
-  body('amount').isNumeric().custom(value => value > 0).withMessage('Bid amount must be positive').escape()
-], placeBid);
+router.post(
+  "/",
+  [
+    body("amount")
+      .isNumeric()
+      .custom((value) => value > 0)
+      .withMessage("Bid amount must be positive")
+      .escape(),
+  ],
+  placeBid,
+);
 
 /**
  * @swagger
@@ -65,9 +78,17 @@ router.post('/', [
  *       400:
  *         description: New amount is lower/equal to current amount.
  */
-router.put('/', [
-  body('amount').isNumeric().custom(value => value > 0).withMessage('Increase bid correctly').escape()
-], updateBid);
+router.put(
+  "/",
+  [
+    body("amount")
+      .isNumeric()
+      .custom((value) => value > 0)
+      .withMessage("Increase bid correctly")
+      .escape(),
+  ],
+  updateBid,
+);
 
 /**
  * @swagger
@@ -79,7 +100,7 @@ router.put('/', [
  *       200:
  *         description: Returns NO_BID, CURRENTLY_WINNING, or LOSING
  */
-router.get('/status', getBidStatus);
+router.get("/status", getBidStatus);
 
 /**
  * @swagger
@@ -91,6 +112,6 @@ router.get('/status', getBidStatus);
  *       200:
  *         description: Returns win count and limit exhaustion tracking
  */
-router.get('/monthly-limit', getMonthlyStatus);
+router.get("/monthly-limit", getMonthlyStatus);
 
 module.exports = router;
